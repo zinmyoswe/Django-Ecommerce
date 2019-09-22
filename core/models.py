fa8edba41f0ffc92c8fe94e364408af39dabe1eb
ZIN MYO <overmidnight12@gmail.com>
2019-09-21 22:30:53 -0700

    my changes

--- core/models.py
+++ core/models.py
@@ -63,6 +63,11 @@ class OrderItem(models.Model):
     def get_amount_saved(self):
         return self.get_total_item_price() - self.get_total_discount_item_price()
 
+    def get_final_price(self):
+        if self.item.discount_price:
+            return self.get_total_discount_item_price()
+        return self.get_total_item_price()
+
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -74,3 +79,9 @@ class Order(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        return total
