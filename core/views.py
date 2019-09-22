9487a9c106b0935ded2d9c062ee16487fe7fbc9a
ZIN MYO <overmidnight12@gmail.com>
2019-09-22 10:44:19 -0700

    my changes

--- core/views.py
+++ core/views.py
@@ -117,3 +117,32 @@ def remove_from_cart(request, slug):
         messages.info(request, "u don't have an active order.")
         return redirect("core:product", slug=slug)
     return redirect("core:product", slug=slug)
+
+
+@login_required
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            order.items.remove(order_item)
+            messages.info(request, "Item was removed from your cart.")
+            return redirect("core:product", slug=slug)
+        else:
+            # add a message saying the user dosent have an order
+            messages.info(request, "Item was not in your cart.")
+            return redirect("core:product", slug=slug)
+    else:
+        # add a message saying the user dosent have an order
+        messages.info(request, "u don't have an active order.")
+        return redirect("core:product", slug=slug)
+    return redirect("core:product", slug=slug)
