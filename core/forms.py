a29a5946bf564d6b4ef3de02f0784addf551f96a
ZIN MYO <overmidnight12@gmail.com>
2019-09-27 11:21:52 -0700

    my changes

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
 
 PAYMENT_CHOICES = (
     ('S', 'Stripe'),
@@ -18,7 +19,7 @@ class CheckoutForm(forms.Form):
         'class': 'custom-select d-block w-100'
     })
     zip = forms.CharField()
-    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
-    save_info = forms.BooleanField(widget=forms.CheckboxInput())
+    same_billing_address = forms.BooleanField(required=False)
+    save_info = forms.BooleanField(required=False)
     payment_option = forms.ChoiceField(
         widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
