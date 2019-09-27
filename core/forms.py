670c217334e1baf1a68a634f52d289cd659ca67c
ZIN MYO <overmidnight12@gmail.com>
2019-09-27 10:19:44 -0700

    my changes

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from django_countries.fields import CountryField
 
+PAYMENT_CHOICES = (
+    ('S', 'Stripe'),
+    ('P', 'PayPal')
+)
+
 
 class CheckoutForm(forms.Form):
     street_address = forms.CharField()
@@ -9,4 +14,5 @@ class CheckoutForm(forms.Form):
     zip = forms.CharField()
     same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
     save_info = forms.BooleanField(widget=forms.CheckboxInput())
-    payment_option = forms.BooleanField(widget=forms.RadioSelect())
+    payment_option = forms.ChoiceField(
+        widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
