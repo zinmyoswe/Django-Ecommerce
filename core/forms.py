b277bc573656a93691bee4e7ef38b559f91b2eb9
ZIN MYO <overmidnight12@gmail.com>
2019-09-27 12:40:04 -0700

    my changes

--- core/forms.py
+++ core/forms.py
@@ -10,15 +10,20 @@ PAYMENT_CHOICES = (
 
 class CheckoutForm(forms.Form):
     street_address = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': '1234 Main St'
+        'placeholder': '1234 Main St',
+        'class': 'form-control'
     }))
     apartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
-        'placeholder': 'Apartment or suite'
+        'placeholder': 'Apartment or suite',
+        'class': 'form-control'
     }))
-    country = CountryField(blank_label='(select country)').formfield(attrs={
+    country = CountryField(blank_label='(select country)').formfield(widget=CountrySelectWidget(attrs={
         'class': 'custom-select d-block w-100'
-    })
-    zip = forms.CharField()
+
+    }))
+    zip = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
     same_billing_address = forms.BooleanField(required=False)
     save_info = forms.BooleanField(required=False)
     payment_option = forms.ChoiceField(
--- core/views.py
+++ core/views.py
@@ -52,6 +52,7 @@ class CheckoutView(View):
     def post(self, *args, **kwargs):
         form = CheckoutForm(self.request.POST or None)
         if form.is_valid():
+            print(form.cleaned_data)
             print("The form is valid")
             return redirect('core:checkout')
 
