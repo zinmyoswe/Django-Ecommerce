a57e99aaa5345d6cf26946bb75dc1d855aa55229
ZIN MYO <overmidnight12@gmail.com>
2019-09-27 17:35:59 -0700

    my changes

--- core/models.py
+++ core/models.py
@@ -90,13 +90,14 @@ class Order(models.Model):
             total += order_item.get_final_price()
         return total
 
-    class BillingAddress(models.Model):
-        user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                                 on_delete=models.CASCADE)
-        street_address = models.CharField(max_length=100)
-        apartment_address = models.CharField(max_length=100)
-        countries = CountryField(multiple=True)
-        zip = models.CharField(max_length=100)
 
-        def __str__(self):
-            return self.user.username
+class BillingAddress(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    street_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
+    country = CountryField(multiple=False)
+    zip = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ from django.views.generic import ListView, DetailView, View
 from django.shortcuts import redirect
 from django.utils import timezone
 from .forms import CheckoutForm
-from .models import Item, OrderItem, Order
+from .models import Item, OrderItem, Order, BillingAddress
 
 # Create your views here.
 
@@ -53,6 +53,22 @@ class CheckoutView(View):
         form = CheckoutForm(self.request.POST or None)
         print(self.request.POST)
         if form.is_valid():
+            street_address = form.cleaned_data.get('street_address')
+            apartment_address = form.cleaned_data.get('apartment_address')
+            country = form.cleaned_data.get('country')
+            zip = form.cleaned_data.get('zip')
+            same_billing_address = form.cleaned_data.get(
+                'same_billing_address')
+            save_info = form.cleaned_data.get('save_info')
+            payment_option = form.cleaned_data.get('payment_option')
+            billing_address = BillingAddress(
+                user=self.request.user,
+                street_address=street_address,
+                apartment_address=apartment_address,
+                country=country,
+                zip=zip
+            )
+            billing_address.save()
             return redirect('core:checkout')
         messages.warning(self.request, "Failed Checkout")
         return redirect('core:checkout')
