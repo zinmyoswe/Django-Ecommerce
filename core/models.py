07c654889c4d2e1e18fed99ae35e5e82d8881a91
ZIN MYO <overmidnight12@gmail.com>
2019-10-01 15:50:17 -0700

    my changes

--- core/models.py
+++ core/models.py
@@ -83,6 +83,8 @@ class Order(models.Model):
         'BillingAddress', on_delete=models.SET_NULL, blank=True, null=True)
     payment = models.ForeignKey(
         'Payment', on_delete=models.SET_NULL, blank=True, null=True)
+    coupon = models.ForeignKey(
+        'Coupon', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.user.username
