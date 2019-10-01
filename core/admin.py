fe967ba713a660a96c302306ccba72c48a739892
ZIN MYO <overmidnight12@gmail.com>
2019-10-01 13:46:19 -0700

    my changes

--- core/admin.py
+++ core/admin.py
@@ -4,7 +4,12 @@ from .models import Item, OrderItem, Order, Payment
 
 # Register your models here.
 
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'ordered']
+
+
 admin.site.register(Item)
 admin.site.register(OrderItem)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Payment)
--- demo/urls.py
+++ demo/urls.py
@@ -10,8 +10,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,
