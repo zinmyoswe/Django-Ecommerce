03b24b26e3f79fe6d156501cfe0cbf9381980e2a
ZIN MYO <overmidnight12@gmail.com>
2019-09-30 19:26:38 -0700

    my changes

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Item, OrderItem, Order
+from .models import Item, OrderItem, Order, Payment
 
 # Register your models here.
 
 admin.site.register(Item)
 admin.site.register(OrderItem)
 admin.site.register(Order)
+admin.site.register(Payment)
--- core/views.py
+++ core/views.py
@@ -23,7 +23,7 @@ class PaymentView(View):
     def post(self, *args, **kwargs):
         order = Order.objects.get(user=self.request.user, ordered=False)
         token = self.request.POST.get('stripeToken')
-        amount = order.get_total() * 100
+        amount = int(order.get_total() * 100)
         try:
             charge = stripe.Charge.create(
                 amount=amount,  # cents
@@ -34,44 +34,55 @@ class PaymentView(View):
             payment = Payment()
             payment.stripe_charge_id = charge['id']
             payment.user = self.request.user
-            payment.amount = amount
+            payment.amount = order.get_total()
             payment.save()
 
             # assign the payment to the order
             order.ordered = True
             order.payment = payment
             order.save()
+
+            messages.success(self.request, "Order was successful")
+            return redirect("/")
+
         except stripe.error.CardError as e:
             # Since it's a decline, stripe.error.CardError will be caught
             body = e.json_body
             err = body.get('error', {})
             messages.error(self.request, f"{err.get('message')}")
+            return redirect("/")
 
         except stripe.error.RateLimitError as e:
             # Too many requests made to the API too quickly
             messages.error(self.request, "RateLimitError")
+            return redirect("/")
 
         except stripe.error.InvalidRequestError as e:
             # Invalid parameters were supplied to Stripe's API
             messages.error(self.request, "Invalid parameters")
+            return redirect("/")
 
         except stripe.error.AuthenticationError as e:
             # Authentication with Stripe's API failed
             # (maybe you changed API keys recently)
             messages.error(self.request, "Not Authentication")
+            return redirect("/")
 
         except stripe.error.APIConnectionError as e:
             # Network communication with Stripe failed
             messages.error(self.request, "Network Error")
+            return redirect("/")
 
         except stripe.error.StripeError as e:
             # Display a very generic error to the user, and maybe send
             # yourself an email
             messages.error(self.request, "Something went wrong")
+            return redirect("/")
 
         except Exception as e:
             # send an email to ourselves
             messages.error(self.request, "Serious Error occured")
+            return redirect("/")
 
 
 class HomeView(ListView):
