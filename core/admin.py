aad0e80ddb456c3a5daf4835317afae1ee37c1ad
ZIN MYO <overmidnight12@gmail.com>
2019-10-04 22:05:04 -0700

    my changes

--- core/admin.py
+++ core/admin.py
@@ -5,6 +5,13 @@ from .models import Item, OrderItem, Order, Payment, Coupon, Refund
 # Register your models here.
 
 
+def make_refund_accepted(modeladmin, request, queryset):
+    queryset.update(refund_requested=False, refund_granted=True)
+
+
+make_refund_accepted.short_description = 'Update orders to refund granted'
+
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['user',
                     'ordered',
@@ -32,6 +39,7 @@ class OrderAdmin(admin.ModelAdmin):
         'user__username',
         'ref_code'
     ]
+    actions = [make_refund_accepted]
 
 
 admin.site.register(Item)
